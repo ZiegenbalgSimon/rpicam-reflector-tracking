034939682e2bb7e0190f842a21e088cf71514c88
SimonZiegenbalg <sziegenbalg04@gmail.com>
2026-04-07 11:27:48 +0200

    Improvements from program testing

--- programs/tracking_cout_vid.sh
+++ programs/tracking_cout_vid.sh
@@ -1,7 +1,7 @@
 mkdir -p ../tracking_videos  # creats output folder if missing
 rpicam-vid -t 30s \
-           -o ../tracking_videos/tracking%03d.mp4 \
+           -o ../tracking_videos/tracking_$(date +%Y%m%d_%H%M%S).mp4 \
            -c ../configuration/configuration_camera.txt \
            --post-process-libs ../rpicam-apps/build \
-           --post-process-file ../configuration/configuration_post_processing/configuration_ethernet.json
+           --post-process-file ../configuration/configuration_post_processing/configuration_cout.json
 # on Raspberry Pi 4 and earlier, output to mp4 container requires --codec libav
